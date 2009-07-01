4180da23a1bf05536c11a42436875c86b1706171
devilfighter1806 <devilfighter1806@d524564c-661d-11de-9867-89bd1bae666e>
2009-07-01 09:08:36 +0000

    Add first source files of the working logic of the game.

---
+++ trunk/ connecting4 --username devilfighter1806/Connect4/src/connect4/Direction.java
@@ -0,0 +1,38 @@
+package connect4;
+
+/**
+ * Represents directions from which user can move man.
+ * 
+ * @author Stanislav Petrov
+ * 
+ */
+public enum Direction {
+
+	TOP(1, 0), BOTTOM(2, 6), LEFT(3, 0), RIGHT(4, 6);
+	/**
+	 * Stores sequential number of the direction.
+	 */
+	private final int number;
+	/**
+	 * Stores position of the direction.
+	 */
+	private final int position;
+
+	/**
+	 * General purpose constructor.
+	 * @param num Sequential number to be set.
+	 * @param position Position to be set.
+	 */
+	Direction(int num, int position) {
+		this.number = num;
+		this.position = position;
+	}
+
+	/**
+	 * Direction position getter.
+	 * @return Row or column of the relevant position.
+	 */
+	public int getPosition() {
+		return this.position;
+	}
+}
---
+++ trunk/ connecting4 --username devilfighter1806/Connect4/src/connect4/GamePlay.java
@@ -0,0 +1,161 @@
+package connect4;
+
+import java.util.Scanner;
+
+import javax.security.auth.callback.TextOutputCallback;
+
+/**
+ * Controls game play.
+ * 
+ * @author Stanislav Petrov
+ * 
+ */
+public class GamePlay {
+	/**
+	 * Stores game mode - single|multi player.
+	 */
+	private GameMode gameMode;
+	/**
+	 * Stores which is current player.
+	 */
+	private char currentPlayer;
+	/**
+	 * Stores connect4 solving algorithms.
+	 */
+	private Connect4Solver connect4;
+	/**
+	 * Reads from standard input.
+	 */
+	private Scanner stdin;
+
+	public GamePlay(GameMode gameMode, int boardSize) {
+		stdin = new Scanner(System.in);
+		this.gameMode = gameMode;
+		connect4 = new Connect4Solver(gameMode, boardSize);
+	}
+
+	/**
+	 * Switches current player's turn in multiplayer mode.
+	 * 
+	 * @param currentPlayer
+	 *            Current player which will be switched.
+	 * @return Current player's turn.
+	 */
+	private char switchPlayer() {
+		if (currentPlayer == Connect4Solver.BLACK) {
+			currentPlayer = Connect4Solver.WHITE;
+		} else {
+			currentPlayer = Connect4Solver.BLACK;
+		}
+		return currentPlayer;
+	}
+
+	/**
+	 * Configures new game.
+	 * 
+	 * @param boardSize
+	 *            Board size to be set.
+	 * @param gameMode
+	 *            Game mode to be set.
+	 */
+	public void configureGame(int boardSize, GameMode gameMode) {
+		// TODO To be implemented.
+	}
+
+	/**
+	 * Game mode setter.
+	 * 
+	 * @param gameMode
+	 *            Game mode to be set.
+	 */
+	public void setGameMode(GameMode gameMode) {
+		this.gameMode = gameMode;
+	}
+
+	/**
+	 * User chooses which player to be and current player becomes oponent.
+	 */
+	public void choosePlayer() {
+		System.out.println("Choose player (x-black, o - white):");
+		this.currentPlayer = stdin.nextLine().charAt(0);
+	}
+
+	/**
+	 * Sets current player who will be the first one.
+	 * 
+	 * @param player
+	 */
+	public void setPlayer(char player) {
+		this.currentPlayer = player;
+	}
+
+	/**
+	 * Determines the game mode and loops the relevant game play.
+	 */
+	public void loopGame() {
+		if (gameMode == GameMode.SIINGLE_PLAYER) {
+			playSinglePlayer();
+		} else {
+			playMultiPlayer();
+		}
+	}
+
+	/**
+	 * Controls the game play in single player mode.
+	 */
+	public void playSinglePlayer() {
+		connect4.setBotPlayer();
+		int position, num;
+		boolean validMove = true;
+		connect4.printBoard();
+		while (true) {
+			System.out.println("Current player is: " + currentPlayer);
+			System.out.println("Position(1-Top, 2-Bottom, 3-Left, 4-Right:");
+			position = stdin.nextInt();
+			System.out.print("Number: ");
+			num = stdin.nextInt();
+			if (position == -1 && num == -1)
+				break;
+			switch (position) {
+			case 1:
+				if(connect4.moveMan(currentPlayer, Direction.TOP, num)==false){
+					System.out.println("Invalid move!");
+				}
+				break;
+			case 2: 
+				validMove = connect4.moveMan(currentPlayer, Direction.BOTTOM, num);
+				break;
+			case 3: 
+				validMove = connect4.moveMan(currentPlayer, Direction.LEFT, num);
+				break;
+			case 4: 
+				validMove = connect4.moveMan(currentPlayer, Direction.RIGHT, num);
+				break;
+			}
+			if(!validMove) System.out.println("Invalid move!");
+			connect4.printBoard();
+		}
+	}
+
+	/**
+	 * Controls the game play in multiplayer mode.
+	 */
+	public void playMultiPlayer() {
+		// TODO To be implemented.
+	}
+
+	/**
+	 * Starts game loop.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// configure new game.
+		GamePlay game = new GamePlay(GameMode.SIINGLE_PLAYER, 7);
+		// choose which player to be.
+		game.choosePlayer();
+		// game loop.
+		game.loopGame();
+	}
+
+}
