9a7fbcf07d3b741509e4eea6029e228129320707
Leni.Kirilov <Leni.Kirilov@d524564c-661d-11de-9867-89bd1bae666e>
2009-07-14 21:13:36 +0000

    Rough implementation of ARROW. It's rotating around the board. Has Left/right/up/down sides. Left click - move left/down. Right click - move right/up. Need some time adjusting the positioning so that the arrow doesn't appear on the MEN.

---  connecting4 --username devilfighter1806/Connect4/src/connect4/view/RotatableSquare.java
+++  connecting4 --username devilfighter1806/Connect4/src/connect4/view/RotatableSquare.java
@@ -7,17 +7,18 @@ import java.awt.Point;
 public class RotatableSquare {
 
 	private Point upLeft;
-	private Point mid;
 	public Point tempUpLeft;
 	private Position position;
 	private int size;
+
+	public Point[] keyPositions;
 
 	public RotatableSquare(int x, int y, int size) {
 		this.size = size;
 		upLeft = new Point(x, y);
 		position = Position.DEGREE_45;
-		mid = new Point(x + size / 2, y + size / 2);
 		tempUpLeft = new Point(upLeft);
+		keyPositions = new Point[4];
 	}
 
 	public void rotateLeft() {
@@ -32,11 +33,27 @@ public class RotatableSquare {
 	}
 
 	public void paint(Graphics g) {
-		int x = mid.x;
-		int y = mid.y;
+		int x = upLeft.x + size / 2;
+		int y = upLeft.y + size / 2;
 		int cos = (int) ((size * Math.cos(Math.PI / 8) * Math.sqrt(2)) / 2);
 		int sin = (int) ((size * Math.sin(Math.PI / 8) * Math.sqrt(2)) / 2);
 		int sqr2 = (int) (size * Math.sqrt(2) / 2);
+
+		for (int i = 0; i < 4; i++) {
+			keyPositions[i] = new Point();
+		}
+		/*
+		 * // 0 keyPositions[0].x = x + sqr2; keyPositions[0].y = y;
+		 * 
+		 * // 22 (nadqsno 1va stapka) keyPositions[1].x = x - sin;
+		 * keyPositions[1].y = y - cos;
+		 * 
+		 * // 45 (nadqsno 2ra stapka) keyPositions[2].x = x; keyPositions[2].y =
+		 * y - sqr2;
+		 * 
+		 * // 77 (nadqsno 3ta stapka) keyPositions[3].x = x + sin;
+		 * keyPositions[3].y = y - cos;
+		 */
 
 		switch (position.i) {
 
@@ -46,33 +63,47 @@ public class RotatableSquare {
 			tempUpLeft.x = 0;
 			tempUpLeft.y = 0;
 
+			g.drawString("case 0", 20, 20);
+
+			g.setColor(Color.BLACK);
 			g.drawLine(x + sqr2, y, x, y + sqr2);
+			g.setColor(Color.RED);
 			g.drawLine(x, y + sqr2, x - sqr2, y);
+			g.setColor(Color.GREEN);
 			g.drawLine(x - sqr2, y, x, y - sqr2);
+			g.setColor(Color.BLUE);
 			g.drawLine(x, y - sqr2, x + sqr2, y);
 			break;
 
 		// 22
 		case 1:
 
-			System.out.println("case 1");
-			tempUpLeft.x = upLeft.x + (int) (sqr2) / 2;
-			tempUpLeft.y = upLeft.y - (int) (sqr2) / 2;
+			g.drawString("case 1", 20, 20);
 
+			System.out.println("case 1");
+			// tempUpLeft.x = upLeft.x + (int) (sqr2) / 2;
+			// tempUpLeft.y = upLeft.y - (int) (sqr2) / 2;
+			tempUpLeft.x = x - sin;
+			tempUpLeft.y = x - cos;
+
+			g.setColor(Color.BLACK);
 			g.drawLine(x + sin, y + cos, x - cos, y + sin);
+			g.setColor(Color.RED);
 			g.drawLine(x - cos, y + sin, x - sin, y - cos);
+			g.setColor(Color.GREEN);
 			g.drawLine(x - sin, y - cos, x + cos, y - sin);
+			g.setColor(Color.BLUE);
 			g.drawLine(x + cos, y - sin, x + sin, y + cos);
 			break;
 
 		// 45
 		case 2:
 
-			System.out.println("case 2");
+//			g.drawString("case 2", 20, 20);
+
+//			System.out.println("case 2");
 			tempUpLeft.x = x - sin;
 			tempUpLeft.y = y - cos;
-
-			System.out.println("tempUL " + tempUpLeft);
 
 			g.drawRect(upLeft.x, upLeft.y, size + 2, size + 2);
 			break;
@@ -85,9 +116,15 @@ public class RotatableSquare {
 			tempUpLeft.x = upLeft.x - (int) (sqr2) / 2;
 			tempUpLeft.y = upLeft.y + (int) (sqr2) / 2;
 
+			g.drawString("case 3", 20, 20);
+
+			g.setColor(Color.BLACK);
 			g.drawLine(x + cos, y + sin, x - sin, y + cos);
+			g.setColor(Color.RED);
 			g.drawLine(x - sin, y + cos, x - cos, y - sin);
+			g.setColor(Color.GREEN);
 			g.drawLine(x - cos, y - sin, x + sin, y - cos);
+			g.setColor(Color.BLUE);
 			g.drawLine(x + sin, y - cos, x + cos, y + sin);
 			break;
 		}
