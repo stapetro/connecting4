22c8db0001c6806b5f367db53b634e5bca575bc3
Leni.Kirilov <Leni.Kirilov@d524564c-661d-11de-9867-89bd1bae666e>
2009-07-15 18:12:01 +0000

    Arrow working with connect4.controller.Direction now.

---  connecting4 --username devilfighter1806/Connect4/src/connect4/view/Arrow.java
+++  connecting4 --username devilfighter1806/Connect4/src/connect4/view/Arrow.java
@@ -1,5 +1,6 @@
 package connect4.view;
 
+import connect4.controller.Direction;
 import java.awt.Graphics;
 import java.awt.Point;
 
@@ -12,7 +13,7 @@ public class Arrow {
 	public Arrow(int size, int x, int y) {
 		this.size = size;
 		startPoint = new Point(x, y);
-		myDirection = Direction.DOWN;
+		myDirection = Direction.VERTICAL_DOWN;
 	}
 
 	public void paint(Graphics g) {
@@ -22,25 +23,25 @@ public class Arrow {
 		
 		//points at 
 		switch (myDirection) {
-		case DOWN:
+		case VERTICAL_DOWN:
 			g.drawLine(x, y, x, y + size);
 			g.drawLine(x, y + size, x - size / 2, y + size / 2);
 			g.drawLine(x, y + size, x + size / 2, y + size / 2);
 			break;
 
-		case UP:
+		case VERTICAL_UP:
 			g.drawLine(x, y, x, y - size);
 			g.drawLine(x, y - size, x - size / 2, y - size / 2);
 			g.drawLine(x, y - size, x + size / 2, y - size / 2);
 			break;
 
-		case LEFT:
+		case HORIZONTAL_LEFT:
 			g.drawLine(x, y, x - size, y);
 			g.drawLine(x - size, y, x - size / 2, y - size / 2);
 			g.drawLine(x - size, y, x - size / 2, y + size / 2);
 			break;
 
-		case RIGHT:
+		case HORIZONTAL_RIGHT:
 			g.drawLine(x, y, x + size, y);
 			g.drawLine(x + size, y, x + size / 2, y - size / 2);
 			g.drawLine(x + size, y, x + size / 2, y + size / 2);
