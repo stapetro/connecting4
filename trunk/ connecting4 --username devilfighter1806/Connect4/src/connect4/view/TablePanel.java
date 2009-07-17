cd0ef085395ce75ae26daf34c512ef702661965d
Leni.Kirilov <Leni.Kirilov@d524564c-661d-11de-9867-89bd1bae666e>
2009-07-17 21:33:01 +0000

    comments for testing removed.

--- trunk/ connecting4 --username devilfighter1806/Connect4/src/connect4/view/TablePanel.java
+++ trunk/ connecting4 --username devilfighter1806/Connect4/src/connect4/view/TablePanel.java
@@ -197,16 +197,9 @@ public class TablePanel extends JPanel {
 	 */
 	public void displayWinningCombination(Point[] winningMenPositions,
 			Color color) {
-		System.out.println("<><><><><><><>");
 		for (Point manPoint : winningMenPositions) {
-			System.out.println("painting " + manPoint + " in "
-					+ color.toString());
 			men[manPoint.x][manPoint.y].setColor(color);
-			System.out.println("men with color"
-					+ men[manPoint.x][manPoint.y].getColor().toString());
 		}
-		System.out.println("<><><><><>repainting<><>");
-
 		repaint();
 	}
 
