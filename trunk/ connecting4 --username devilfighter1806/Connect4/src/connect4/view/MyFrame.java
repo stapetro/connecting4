93d6e65b27521fd0af831d08297b05d66ea86801
Leni.Kirilov <Leni.Kirilov@d524564c-661d-11de-9867-89bd1bae666e>
2009-07-18 00:48:27 +0000

    TablePanel - added paintNumbers - nubmers around the panel which move away from the arrow as it passes by.

--- trunk/ connecting4 --username devilfighter1806/Connect4/src/connect4/view/MyFrame.java
+++ trunk/ connecting4 --username devilfighter1806/Connect4/src/connect4/view/MyFrame.java
@@ -22,15 +22,15 @@ public class MyFrame extends JFrame {
 		statusBar = new StatusBarPanel();
 		hei = h;
 		wid = w;
-		drawPanel = new TablePanel(wid, hei, 13, Color.RED);
+		drawPanel = new TablePanel(wid, hei, 17, Color.RED);
 		init();
 	}
 
 	private void init() {
-		setLayout(new BorderLayout());
+//		setLayout(new BorderLayout());
 		add(drawPanel, BorderLayout.CENTER);
-		drawPanel.setLayout(new BorderLayout());
-		drawPanel.add(statusBar, BorderLayout.SOUTH);
+//		drawPanel.setLayout(new BorderLayout());
+		add(statusBar, BorderLayout.SOUTH);
 
 		setTitle("NextGen Connect4");
 		setSize(hei, wid);
@@ -38,7 +38,7 @@ public class MyFrame extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		statusBar.setStatus("bla", Color.cyan);
 	}
 
