7e87f460f9c6e82487cde2d2a0f7482188c31122
devilfighter1806 <devilfighter1806@d524564c-661d-11de-9867-89bd1bae666e>
2009-07-18 02:31:13 +0000

    Package controller documented.

---  connecting4 --username devilfighter1806/Connect4/src/connect4/model/GameProperties.java
+++  connecting4 --username devilfighter1806/Connect4/src/connect4/model/GameProperties.java
@@ -8,9 +8,9 @@ package connect4.model;
  */
 public enum GameProperties {
 
-	SIZE_9(9, 0), SIZE_13(13, 1);
+	SIZE_9(9, 0), SIZE_11(11, 1), SIZE_13(13, 2), SIZE_15(15, 3);
 
-	public static  final String[] SIZES = {"9x9", "13x13"};
+	public static  final String[] SIZES = {"9x9", "11x11", "13x13", "15x15"};
 	public static final String[] PLAYERS = {"1st player", "2nd player"};
 	private final int size;
 	private final int nameId;
